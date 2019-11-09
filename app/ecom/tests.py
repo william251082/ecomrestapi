a48e8854d0dd9a59bd04d0c67161d5c1e3d49163
william <william.del.rosario@iceshop.nl>
2019-11-09 21:45:08 +0100

    Tests, Models and serializers for product and owner

--- app/ecom/tests.py
+++ app/ecom/tests.py
@@ -1,3 +1,34 @@
+from datetime import datetime
+
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Product
+
+
+class ProductModelTest(TestCase):
+    def setUp(self):
+        Product.objects.create(
+            name="ipad",
+            description="Sedutperspiciatis",
+            body="sfgfdgsfdgsfdgs",
+            location="sdffggf",
+            active=True,
+            owner_id=1,
+            release_date=datetime.now()
+        )
+        Product.objects.create(
+            name="ipad",
+            description="Sedutperspiciatis",
+            body="sfgfdgsfdgsfdgs",
+            location="sdffggf",
+            active=True,
+            owner_id=1,
+            release_date=datetime.now()
+        )
+
+    def test_string_representation(self):
+        product = Product(name="My product name")
+        self.assertEqual(str(product), product.name)
+
+    def test_verbose_name_plural(self):
+        self.assertEqual(str(Product._meta.verbose_name_plural), "products")
